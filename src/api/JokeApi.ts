92712a6492a785cbd00d9c1e770144a246d9e3d7
Yigal Akselman <yakselman@watsco.com>
2022-07-30 14:06:45 -0400

    impl JokeApi getRandomJoke function and test

---
+++ src/api/JokeApi.ts
@@ -0,0 +1,11 @@
+import axios from "axios"
+
+export interface GetRandomJokeRes {
+    id: number
+    joke: string
+}
+
+export const getRandomJoke = async (): Promise<GetRandomJokeRes> => {
+    const response = await axios.get<GetRandomJokeRes>("https://v2.jokeapi.dev/joke/Any?safe=true&type=single")
+    return response.data
+}
---
+++ test/api/JokeApi.spec.ts
@@ -0,0 +1,54 @@
+import { afterAll, afterEach, beforeAll, describe, it, expect } from 'vitest'
+import { setupServer } from 'msw/node'
+import { rest } from 'msw'
+import {getRandomJoke} from "../../src/api/JokeApi"
+
+const joke = {
+    error: false,
+    category: "Misc",
+    type: "single",
+    joke: "My wife and I have reached the difficult decision that we do not want children. If anybody does, please just send me your contact details and we can drop them off tomorrow.",
+    flags: {
+        nsfw: false,
+        religious: false,
+        political: false,
+        racist: false,
+        sexist: false,
+        explicit: false
+    },
+    id: 257,
+    safe: true,
+    lang: "en"
+}
+
+export const restHandlers = [
+    rest.get('https://v2.jokeapi.dev/joke/Any?safe=true&type=single', (req, res, ctx) => {
+        const safe = req.url.searchParams.get("safe")
+        const type = req.url.searchParams.get("type")
+        if(safe === 'true' && type === 'single'){
+            return res(ctx.status(200), ctx.json(joke))
+        }
+        else {
+            return res(ctx.status(500), ctx.json({message: "Not Implemented"}))
+        }
+    }),
+]
+
+const server = setupServer(...restHandlers)
+
+// Start server before all tests
+beforeAll(() => server.listen({ onUnhandledRequest: 'error' }))
+
+//  Close server after all tests
+afterAll(() => server.close())
+
+// Reset handlers after each test `important for test isolation`
+afterEach(() => server.resetHandlers())
+
+describe("JokeApi", () => {
+    it('should return a random joke', async () => {
+        const getRandomJokeRes = await getRandomJoke()
+        expect(getRandomJokeRes.id).toBe(joke.id)
+        expect(getRandomJokeRes.joke).toBe(joke.joke)
+    })
+})
